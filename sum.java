6b2e87bd17e2ff6805487fbd96b02092ca37a1a4
devopsnhc20 <126224045+devopsnhc20@users.noreply.github.com>
2023-02-28 11:08:29 +0530

    first commit

---
+++ sum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class sum
+ {
+
+    public static void main(String[] args)
+    {        
+        int a,b,sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter First Number:");
+        b = sc.nextInt();
+        
+        System.out.println("Enter Second Number:");
+        a = sc.nextInt();
+        
+        sc.close();
+        sum = a+b;
+        System.out.println("Sum of these numbers:"+sum);
+    }
+}
